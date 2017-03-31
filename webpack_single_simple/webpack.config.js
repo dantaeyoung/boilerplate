1f343f5302c840f43766bcc71d5248979f8132da
Dan Taeyoung <provolot@gmail.com>
2017-03-31 16:35:23 -0400

    better webpack config

--- webpack_single_simple/webpack.config.js
+++ webpack_single_simple/webpack.config.js
@@ -1,17 +1,30 @@
 const path = require('path');
+const webpack = require('webpack');
 var HtmlWebpackPlugin =  require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-
 const config = {
   entry: './site/main.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
+    output: {
+    path: __dirname + '/dist', //where it puts build files
+    publicPath: '/', // where it links to within the code
     filename: 'bundle.js'
   },
-  devtool: 'eval',
+  devServer: {
+    inline: true,
+    hot: true
+  },
+  devtool: 'source-map',
   module: {
-    loaders: [
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/,
+        query: {
+          presets: ['es2015']
+        }
+      },
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
@@ -21,17 +34,22 @@ const config = {
             'sass-loader?outputStyle=expanded'
           ]
         })
-			}	
+            }    ,
+      { test: /\.(png|jpg|svg|json)$/, loader: 'file-loader?name=[name].[ext]' }
+
     ]
   },
-	plugins: [
-		new HtmlWebpackPlugin({
+    plugins: [
+        new HtmlWebpackPlugin({
       template: './site/index.html'
     }),
-    new ExtractTextPlugin('bundle.css')
-	]	
+    new ExtractTextPlugin('bundle.css'),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    })
+    ]    
 };
 
 module.exports = config;
-
 
